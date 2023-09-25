276e4ace9d17a4df3671526572723807e8a0d6f7
Andrew Stewart Gibson <andrew@goofballLogic.com>
2023-09-25 10:24:14 +0100

    Some unnecessary code crept in

--- articles/002_7-Messages-CSharp/Taxonomy/CartTotalsUpdated.cs
+++ articles/002_7-Messages-CSharp/Taxonomy/CartTotalsUpdated.cs
@@ -2,8 +2,5 @@ namespace Messages.Taxonomy
 {
     public record CartTotalsUpdated(int ItemCount, decimal TotalCost)
         : Message("Cart totals updated") {
-
-        // public CartTotalsUpdated(CartTotalsUpdated cartTotalsUpdated)
-        //     : base(cartTotalsUpdated) { }
     }
 }
